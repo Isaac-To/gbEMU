51a0427e0bd4c5929cd8acd6f64bf837d3258f4f
Isaac-To <isaacto3890@gmail.com>
2024-08-05 05:44:58 +0000

    add memory and vram

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -3,11 +3,22 @@ mod reg;
 
 pub struct CPU {
     // Registers
+    // PC and SP are 8,9 and 10,11 respectively
     registers: [u8; 12],
+
+    // Memory
+    memory: [u8; 8192],
+
+    // Video memory
+    vram: [u8; 8192],
 }
 
 impl CPU {
     pub fn new() -> CPU {
-        CPU { registers: [0; 12] }
+        CPU {
+            registers: [0; 12],
+            memory: [0; 8192],
+            vram: [0; 8192],
+        }
     }
 }
